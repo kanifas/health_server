a0d80420112b018192fdaddccd24fedc4141488c
Dima Klimov <d.klimov@sptlab.ru>
2023-05-19 19:08:06 +0300

    Added CORS Allow-Origin for render.com client

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-  origin: `${process.env.CLIENT_URL}:${process.env.CLIENT_PORT}`
+
+// app.use(cors({
+//   credentials: true,
+//   origin: `${process.env.CLIENT_URL}:${process.env.CLIENT_PORT}`
+// }));
+app.use(app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://healthclient.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
 }));
+
 app.use('/api', router);
 app.use(errorMiddleware);
 
